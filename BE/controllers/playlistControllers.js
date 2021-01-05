6523e1754fcaf01f3633c4088c0153ddd3e53f8e
cwc329 <iaiminwtg@gmail.com>
2021-01-05 12:35:57 +0800

    add BE controllers todos

---
+++ BE/controllers/playlistControllers.js
@@ -0,0 +1,5 @@
+//TODO add a playlist
+//TODO delete a playlist
+//TODO add episode to playlist
+//TODO remove episode from playlist
+//TODO change playlist name
---
+++ BE/controllers/podcastifyControllers.js
@@ -0,0 +1,2 @@
+//TODO subscribe
+//TODO unsubscribe
