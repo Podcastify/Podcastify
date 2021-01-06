933b6db55518195d882f6fa54e847a6f2912aec0
cwc329 <iaiminwtg@gmail.com>
2021-01-07 04:12:28 +0800

    Added addPlaylist and getPlaylists functions. Added router me

--- BE/controllers/playlistControllers.js
+++ BE/controllers/playlistControllers.js
@@ -1,5 +1,77 @@
-//TODO add a playlist
 //TODO delete a playlist
+//TODO change playlist name
 //TODO add episode to playlist
 //TODO remove episode from playlist
-//TODO change playlist name
+
+const db = require('../models');
+const { Users, Podcasts, Playlists, Episodes, Subscriptions, Records } = db;
+
+const addPlaylist = async (req, res, next) => {
+  const { id } = req.jwtData;
+  const limit = 1;
+  let playlistNumber;
+  try {
+    playlistNumber = await Playlists.count(
+      {
+        where: {
+          userId: id
+        }
+      }
+    )
+  } catch (err) {
+    res.locals.error = err;
+    return res.status(500).send(JSON.stringify(res.locals))
+  }
+  if (playlistNumber >= limit) {
+    res.locals.errorMessage =
+      'Number of playlists has reached limit, you may not add another one.';
+    return res.send(JSON.stringify(res.locals))
+  }
+  const { name } = req.body;
+  let newPlaylist;
+  try {
+    newPlaylist = await Playlists.create(
+      {
+        name,
+        userId: id
+      }
+    )
+  } catch (err) {
+    res.locals.error = err.errors[0].message; //FIXME catch err
+    return res.status(500).send(JSON.stringify(res.locals))
+  }
+  next();
+}
+
+const getPlaylists = async (req, res, next) => {
+  const { id } = req.jwtData;
+  let playlists;
+  try {
+    playlists = await Playlists.findAll(
+      {
+        where: {
+          userId: id
+        },
+        attributes: ['id', 'name'],
+        include: {
+          model: Episodes,
+          attributes: ['id'],
+          through: {
+            attributes: []
+          }
+        }
+      }
+    )
+  } catch (err) {
+    res.locals.error = err;
+    return res.status(500).send(JSON.stringify(res.locals))
+  }
+  res.locals.data = playlists;
+  next();
+}
+
+const deletePlaylist = async (req, res, next) => {
+
+}
+
+module.exports = { addPlaylist, getPlaylists };
--- BE/routes/me.js
+++ BE/routes/me.js
@@ -4,11 +4,29 @@
 
 var express = require('express');
 var router = express.Router();
+const { addPlaylist, getPlaylists } = require('../controllers/playlistControllers');
+const { getMe } = require('../controllers/userControllers');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', getMe, function (req, res, next) {
   res.locals.ok = true;
-  res.send(JSON.stringify(res.locals))
+  res.send(JSON.stringify(res.locals));
 });
+
+router.get('/playlist', getPlaylists, function (req, res, next) {
+  res.locals.ok = true;
+  res.send(JSON.stringify(res.locals));
+});
+
+router.post('/playlist', addPlaylist, function (req, res, next) {
+  res.locals.ok = true;
+  res.send(JSON.stringify(res.locals));
+});
+
+router.delete('/playlist/:id', function (req, res, next) {
+  res.locals.ok = true;
+  res.locals.playlistId = req.params.id;
+  res.send(JSON.stringify(res.locals));
+})
 
 module.exports = router;
