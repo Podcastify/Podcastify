f5a303dff6566a08e05b9e6e29e6cf7ae8e07c75
Yu <loveboice520@gmail.com>
2021-01-12 14:29:07 +0800

    Music Player Layout

---
+++ front_end/src/components/Images.js
@@ -0,0 +1,23 @@
+import { ReactComponent as PlayBtn } from "../images/Play_button_default.svg";
+import { ReactComponent as PauseBtn } from "../images/Pause_button_default.svg";
+import { ReactComponent as NextBtn } from "../images/Next_default.svg";
+import { ReactComponent as PreviousBtn } from "../images/Previous_default.svg";
+import { ReactComponent as PlaylistBtn } from "../images/Playlist_button_default.svg";
+import { ReactComponent as SoundOn } from "../images/Sound_default.svg";
+import { ReactComponent as Mute } from "../images/Mute_default.svg";
+import { ReactComponent as Avatar } from "../images/User_default.svg";
+import { ReactComponent as PodcastifyLogo } from "../images/Logo.svg";
+import { ReactComponent as ListenApiLogo } from "../images/Logo_power_by_Listennotes.svg";
+
+export default {
+  PlayBtn,
+  PauseBtn,
+  NextBtn,
+  PreviousBtn,
+  PlaylistBtn,
+  SoundOn,
+  Mute,
+  Avatar,
+  PodcastifyLogo,
+  ListenApiLogo,
+};
---
+++ front_end/src/constants/breakpoints.js
@@ -0,0 +1,3 @@
+export const MEDIA_QUERY_LG = "@media screen and (min-width: 768px)";
+export const MEDIA_QUERY_MD =
+  "@media screen and (min-width: 481px) and (max-width: 767px)";
