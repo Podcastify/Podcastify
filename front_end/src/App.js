231881274a71ee4ef8943bda992d220875a77f17
cwc329 <iaiminwtg@gmail.com>
2021-01-14 06:44:54 +0800

    modified UserForm and UserInput

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,29 @@
 import Navbar from "./components/Navbar";
 import MusicPlayer from "./components/MusicPlayer";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import Register from './pages/Register';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #3E3A39;
+  }
+  body::after {
+    content: "";
+    
+  }
+  body::before {
+    content: "";
+    background: transparent linear-gradient(180deg, #F15A24AD 3%, #DC52219E 8%, #8E351566 28%, #511E0C3B 46%, #240E051A 63%, #0A040108 77%, #00000000 87%) 0% 0% no-repeat padding-box;
+    mix-blend-mode: screen;
+    opacity: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: fixed;
+    z-index: -99;   
+  }
+`
 
 function App() {
   const theme = {
@@ -12,10 +35,10 @@ function App() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <MusicPlayer />
-    </ThemeProvider>
+    <div>
+      <GlobalStyle />
+      <Register />
+    </div>
   );
 }
 
--- front_end/src/components/UserForm.js
+++ front_end/src/components/UserForm.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Input from './UserInput';
+import useInputs from '../hooks/useInputs';
 const StyledForm = styled.form`
   max-width: 522px;
-  background: linear-gradient(#F15A24AD, black);;
   font-family: Helvetica;
 `;
 
@@ -16,16 +16,67 @@ const StyledFormTitle = styled.h1`
   font-weight: normal;
 `
 
-export default function UserForm() {
+export default function UserForm({ formTitle, className }) {
+  const formInputs = [
+    {
+      attributes: {
+        type: 'text',
+        name: 'username',
+        id: 'username',
+        placeholder: '帳號',
+        required: true,
+        value: '',
+      },
+      title:'',
+      errorMessage: '',
+    },
+    {
+      attributes: {
+        type: 'password',
+        name: 'password',
+        id: 'password',
+        placeholder: '密碼',
+        required: true,
+        value: '',
+      },
+      title:'',
+      errorMessage: '',
+    },
+    {
+      attributes: {
+        type: 'password',
+        name: 'password_double_check',
+        id: 'password_doucle_check',
+        placeholder: '再次輸入密碼',
+        required: true,
+        value: '',
+      },
+      title:'',
+      errorMessage: '',
+    },
+    {
+      attributes: {
+        type: 'submit',
+        name: 'submit',
+        id: 'submit',
+        required: true,
+        value: '確認註冊',
+      },
+      title:'',
+      errorMessage: '',
+    },
+  ]
+
+  const { inputs, handlers } = useInputs(formInputs);
 
   return (
-    <StyledForm>
-      <StyledFormTitle>FORM</StyledFormTitle>
-      <Input />
-      <Input />
-      <Input />
-      <Input type="submit" />
-      <Input type="button" />
+    <StyledForm className={className}>
+      <StyledFormTitle>{formTitle}</StyledFormTitle>
+      {inputs.map(input => <Input
+        {...input}
+        handlers={handlers}
+        key={input.attributes.name}
+      />)}
     </StyledForm>
   )
 }
