f1e9e5bf1d2b885c31f6e4ad09fb4e6c7d330706
cwc329 <test@test.com>
2021-02-02 02:00:16 +0800

    add user context

---
+++ front_end/src/hooks/useUser.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { UserContext } from "../context/context";
+
+export default function useUser() {
+  const { user, setUser } = useContext(UserContext);
+  return { user, setUser }
+}
