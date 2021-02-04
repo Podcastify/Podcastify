8689a466a4570c526f9abc5ab8cec592dd42bef5
cwc329 <test@test.com>
2021-02-04 23:25:00 +0800

    page structure done

--- front_end/src/pages/UserManagement.js
+++ front_end/src/pages/UserManagement.js
@@ -5,6 +5,7 @@ import { UserContext } from "../context/context";
 import UserForm from "../components/UserForm"
 import Navbar from "../components/Navbar";
 import MusicPlayer from "../components/MusicPlayer";
+import { BtnContainer, BtnLogInContainer, BtnLogIn, ButtonName } from "../components/ButtonGroup"
 
 const Container = styled.div`
   width: 100%;
@@ -13,7 +14,50 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const SectionContainer = styled(Container)`
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  overflow-y: unset;
+  align-items: stretch;
+`
 
+const PageTitle = styled.h1`
+  color: #ffffff;
+  font-size: 4rem;
+  font-weight: normal;
+`
+
+const TitleContainer = styled(SectionContainer)`
+  height:auto;
+  flex-direction: row;
+  align-items: center;
+`
+
+const ManageButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  width: 160px;
+  height: 45px;
+  border: 1px solid ${(props) => props.theme.primary_color_grey};
+  border-radius: 3px;
+  color: ${(props) => props.theme.primary_color};
+  margin-left: 25px;
+  font-size: 15px;
+  padding: 10px 32px;
+
+  &:hover {
+    border-color: transparent;
+    background-color: ${(props) => props.theme.hover_color};
+  }
+
+  &:active {
+    border-color: transparent;
+    background-color: ${(props) => props.theme.click_color};
+  }
+`;
 
 export default function UserManagement() {
 
@@ -63,10 +107,19 @@ export default function UserManagement() {
       <Navbar />
       <Main>
         <Div>
-          <UserForm
-            formTitle={"會員資料管理"}
-            formInputs={formInputs}
-          />
+          <SectionContainer>
+            <TitleContainer>
+              <PageTitle>會員資料</PageTitle>
+              <ManageButton>
+                管理我的帳戶
+              </ManageButton>
+              <ManageButton>
+                確認修改
+            </ManageButton>
+            </TitleContainer>
+            <UserForm formInputs={formInputs}>
+            </UserForm>
+          </SectionContainer>
         </Div>
       </Main>
       <MusicPlayer />
