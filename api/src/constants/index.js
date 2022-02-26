ac8ae50876df21c0ca5ab61094fbe11770e038cd
cwc329 <iaiminwtg@gmail.com>
2022-02-26 21:05:23 +0800

    test deploy

--- api/src/constants/index.js
+++ api/src/constants/index.js
@@ -2,6 +2,6 @@ import {config} from 'dotenv';
 config();
 
 const {env} = process;
-export const {API_PORT} = env;
+export const API_PORT = env.API_PORT ?? env.PORT ?? 80;
 export const {LISTEN_KEY} = env;
 export const {SECRET} = env;
