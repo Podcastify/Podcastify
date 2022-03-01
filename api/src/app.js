686d4b49666eb7dbce23846f29087810540390f8
cwc329 <iaiminwtg@gmail.com>
2022-03-01 22:06:10 +0800

    cnofigure cors

--- api/src/app.js
+++ api/src/app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({preflightContinue: false}));
 app.use(express.static(join(__dirname, 'public')));
 app.use(init);
 
